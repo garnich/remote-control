d6c0ea9ad8d4fa4b4fdde80c786e11179aed431d
Andrei Ihnatsiuk <andrei_ihnatsiuk@epam.com>
2022-06-22 12:44:32 +0300

    remote-control: added constants

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+const WRONG_PARAM_MSG = 'wrong parametre';
+const MOUSE = {
+    UP: 'mouse_up',
+    DOWN: 'mouse_down',
+    LEFT: 'mouse_left',
+    RIGHT: 'mouse_right',
+    POS: 'mouse_position'
+};
+
+const STATIC_SERVER_START_MSG = 'HTTP server started on port: ';
+const WS_SERVER_START_MSG = 'Websocket server started on port: ';
+const CLIENT_ERROR_MSG = 'HTTP server err ';
+const WS_ERROR_MSG = 'Websocket server err ';
+
+export { WRONG_PARAM_MSG, MOUSE, STATIC_SERVER_START_MSG, WS_SERVER_START_MSG, CLIENT_ERROR_MSG, WS_ERROR_MSG };
