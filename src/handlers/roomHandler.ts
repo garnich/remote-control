f965673cb8ea3ce43893cdfd2a2fd36847fdaa3f
Andrei Ihnatsiuk <garnich@tut.by>
2024-02-21 19:22:09 +0300

    battleship: added roomHandler

---
+++ src/handlers/roomHandler.ts
@@ -0,0 +1,72 @@
+import DBStorage from '../db/storage';
+import { Server } from 'ws';
+import { WebSocketWithID } from "../http_server";
+
+export const roomHandler = (ws: WebSocketWithID, wss: Server): void => {
+  const indexRoom = DBStorage.createRoom();
+
+  ws.send(JSON.stringify({
+    type: "add_user_to_room",
+    data: JSON.stringify({
+      indexRoom,
+    }),
+    id: 0
+  }));
+
+  const userName = DBStorage.getUserById(ws.id as number)?.name;
+
+  if(userName) {
+    DBStorage.updateRoom(ws.id as number, userName);
+  }
+    const roomsWithOneUser = DBStorage.getRoomsWithOneUser();
+
+    if (roomsWithOneUser.length) {
+      wss.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            type: 'update_room',
+            data: JSON.stringify(roomsWithOneUser),
+            id: 0,
+          })
+        );
+      });
+    }
+};
+
+export const joinRoomHandler = (messageData: string, ws: WebSocketWithID, wss: Server) => {
+  const getData = (msg: string): { indexRoom: number } => JSON.parse(msg);
+
+  const { indexRoom } = getData(messageData);
+
+  const activeRoom = DBStorage.getRoomById(indexRoom);
+
+  const singlePlayer = activeRoom?.roomUsers[0];
+
+  if(singlePlayer && singlePlayer?.index !== ws.id) {
+    const secondPlayer = DBStorage.getUserById(ws.id as number);
+
+    const { idGame } = DBStorage.createGame([{ name: singlePlayer.name, id: singlePlayer.index }, secondPlayer!], indexRoom);
+
+    wss.clients.forEach((client: WebSocketWithID) => {
+      if(client.id === ws.id) {
+        client.send(JSON.stringify({
+          type: "create_game",
+          data: JSON.stringify({
+            idGame,
+            idPlayer: ws.id,
+          }),
+          id: 0
+        }));
+      } else if (client.id === singlePlayer.index) {
+        client.send(JSON.stringify({
+          type: "create_game",
+          data: JSON.stringify({
+            idGame,
+            idPlayer: singlePlayer.index,
+          }),
+          id: 0
+        }));
+      }
+    });
+  }
+};
